792da5865ca0eb8acc08bc956b4a1f53e052b5d0
dwakeherrera <deivid.wgh@gmail.com>
2018-10-16 09:18:33 +0200

    CCategoria implementada actionRefresh (modificando TreeViewHelper.Fill)

--- CSerpisGtk/CSerpisGtk/TreeViewHelper.cs
+++ CSerpisGtk/CSerpisGtk/TreeViewHelper.cs
@@ -5,25 +5,28 @@ using System.Collections;
 namespace Serpis.Ad {
     public class TreeViewHelper {
 		public static void Fill(TreeView treeView, string[] propertyNames, IEnumerable enumerable) {
-			CellRendererText cellRendererText = new CellRendererText();
+			if (treeView.Model == null)
+				init(treeView, propertyNames);
+			ListStore listStore = (ListStore)treeView.Model;
+			listStore.Clear();
+			foreach (object obj in enumerable)
+                listStore.AppendValues(obj);
+		}
 
+		private static void init(TreeView treeView, string[] propertyNames) {
+			CellRendererText cellRendererText = new CellRendererText();
 			foreach (string property in propertyNames) {
                 treeView.AppendColumn(
-                    property,
-                    cellRendererText,
-                    delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
-                        object model = tree_model.GetValue(iter, 0);
-                        object value = model.GetType().GetProperty(property).GetValue(model);
-                        cellRendererText.Text = value + "";
-                    }
-                );
+                property,
+                cellRendererText,
+                delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
+                    object model = tree_model.GetValue(iter, 0);
+                    object value = model.GetType().GetProperty(property).GetValue(model);
+                    cellRendererText.Text = value + "";
+                });
             }
-   
-            ListStore listStore = new ListStore(typeof(object));
-            treeView.Model = listStore;
 
-			foreach (object obj in enumerable)
-				listStore.AppendValues(obj);
-		}
+            treeView.Model = new ListStore(typeof(object));
+        }
     }
 }
