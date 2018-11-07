efefee7dabc2a7bb1b407e207fa5e326c848ac8a
dwakeherrera <deivid.wgh@gmail.com>
2018-11-07 08:54:53 +0100

    Incorporado cambio en EntityDao Enumerable para asignar null

--- dbprueba.sql
+++ dbprueba.sql
@@ -6,3 +6,17 @@ create table categoria (
 insert into categoria (nombre) values ('categoria1');
 insert into categoria (nombre) values ('categoria2');
 insert into categoria (nombre) values ('categoria3');
+
+create table articulo (
+ id serial primary key,
+ nombre varchar(50) not null unique,
+ precio numeric(10,2) not null,
+ categoria bigint unsigned
+);
+
+alter table articulo add foreign key (categoria) references categoria (id);
+
+insert into articulo (nombre, precio, categoria) values ('artículo 1', 1.0, 1);
+insert into articulo (nombre, precio, categoria) values ('artículo 2', 2.0, 2);
+insert into articulo (nombre, precio, categoria) values ('artículo 3', 3.0, 3);
+insert into articulo (nombre, precio) values ('artículo 4', 4.0);
