5b9531609439207b3cb0432a44835643da7eae6a
dwakeherrera <deivid.wgh@gmail.com>
2018-11-23 10:36:11 +0100

    Añadido GConnection mostrando insert into categoria

--- GMySql/src/main/java/serpis/ad/Prueba.java
+++ GMySql/src/main/java/serpis/ad/Prueba.java
@@ -2,15 +2,22 @@ package serpis.ad;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
 
 public class Prueba {
 
+	public static Connection connection;
 	public static void main(String[] args) throws SQLException {
-		//Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/dbprueba?user=root&password=sistemas");
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/dbprueba", "root", "sistemas");
+		connection = DriverManager.getConnection("jdbc:mysql://localhost/dbprueba", "root", "sistemas");
+
+		//insert();
+		//delete(1);
+		//load();
+
 		Statement statement = connection.createStatement();
 		ResultSet resultSet = statement.executeQuery("select * from categoria");
 		while (resultSet.next())
@@ -19,4 +26,30 @@ public class Prueba {
 		connection.close();
 	}
 
+	private static void insert() throws SQLException {
+		String insertSql = "insert into categoria (nombre) values (?)";
+		PreparedStatement preparedStatement = connection.prepareStatement(insertSql);
+		preparedStatement.setObject(1, "categoria " + LocalDateTime.now());
+		preparedStatement.executeUpdate();
+		preparedStatement.close();
+	}
+
+	private static void delete(int id) {
+		String deleteSql = "delete from categoria where id = ?";
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(deleteSql);
+			preparedStatement.setInt(1, id);
+			preparedStatement.executeUpdate();
+			preparedStatement.close();
+		} catch (SQLException e) {
+			//System.out.println(e);
+			System.out.println("No se puede eliminar porque existe una foreignkey");
+		}
+	}
+
+	/*private static void load() throws SQLException {
+		String loadSql = "select * from categoria";
+		PreparedStatement preparedStatement = connection.prepareStatement(loadSql);
+		//TODO
+	}*/
 }
