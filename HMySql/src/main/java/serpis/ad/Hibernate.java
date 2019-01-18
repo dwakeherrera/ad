f9f99b851c7cf6ed0f1228b347a8b2045f509ad5
dwakeherrera <deivid.wgh@gmail.com>
2019-01-18 08:18:01 +0100

    dbprueba.sql: añadidas cliente, pedido y pedidolinea

---
+++ HMySql/src/main/java/serpis/ad/Hibernate.java
@@ -0,0 +1,29 @@
+package serpis.ad;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Hibernate {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("serpis.ad.hmysql");
+
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+
+		List<Categoria> categorias = entityManager.createQuery("from Categoria", Categoria.class).getResultList();
+		for (Categoria categoria : categorias) {
+			System.out.printf("%3d %s%n", categoria.getId(), categoria.getNombre());
+		}
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+
+		entityManagerFactory.close();
+
+	}
+
+}
