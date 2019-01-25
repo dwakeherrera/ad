0686ecb21529ca3aa3f42994aac9e475b26275b0
dwakeherrera <deivid.wgh@gmail.com>
2019-01-25 09:17:37 +0100

    Iniciado HPedido con Categoria y Articulo

--- HMySql/src/main/java/serpis/ad/Hibernate.java
+++ HMySql/src/main/java/serpis/ad/Hibernate.java
@@ -1,5 +1,6 @@
 package serpis.ad;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -10,20 +11,21 @@ public class Hibernate {
 
 	public static void main(String[] args) {
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("serpis.ad.hmysql");
-
+		
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-
+		
+		Categoria nuevaCategoria = new Categoria();
+		nuevaCategoria.setNombre("nueva " + LocalDateTime.now());
+		entityManager.persist(nuevaCategoria);
 		List<Categoria> categorias = entityManager.createQuery("from Categoria", Categoria.class).getResultList();
-		for (Categoria categoria : categorias) {
-			System.out.printf("%3d %s%n", categoria.getId(), categoria.getNombre());
-		}
-
+		for (Categoria categoria : categorias)
+			System.out.printf("%3d %s %n", categoria.getId(), categoria.getNombre());
+		
 		entityManager.getTransaction().commit();
 		entityManager.close();
-
+		
 		entityManagerFactory.close();
-
 	}
 
 }
--- HPedido/src/main/java/serpis/ad/Categoria.java
+++ HPedido/src/main/java/serpis/ad/Categoria.java
@@ -24,4 +24,9 @@ public class Categoria {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	@Override
+	public String toString() {
+		return String.format("[%s] -%s", id, nombre);
+	}
 }
